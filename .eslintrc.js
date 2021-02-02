78674809dd7cd3ea797775fd175e08dcdba58463
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2021-02-02 09:58:26 +0300

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,18 @@ module.exports = {
     },
     "rules": {
         "prettier/prettier": ["error", { "singleQuote": true }],
-        "object-curly-spacing": ["error", "always"]
+        "object-curly-spacing": ["error", "always"],
+        "spaced-comment": "off",
+        "no-console": "warn",
+        "consistent-return": "off",
+        "func-names": "off",
+        "object-shorthand": "off",
+        "no-process-exit": "off",
+        "no-param-reassign": "off",
+        "no-return-await": "off",
+        "no-underscore-dangle": "off",
+        "class-methods-use-this": "off",
+        "prefer-destructuring": ["error", { "object": true, "array": false }],
+        "no-unused-vars": ["error", { "argsIgnorePattern": "req|res|next|val" }]
     }
 };
