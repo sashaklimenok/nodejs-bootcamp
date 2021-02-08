bb41893d306cccf647c6f6ce55f99a0d11393907
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2021-02-08 12:43:35 +0300

    refactoring architecture

---
+++ src/Controller/TourController.js
@@ -0,0 +1,17 @@
+const getAllTours = (req, res) => {};
+
+const getTour = (req, res) => {};
+
+const createTour = (req, res) => {};
+
+const updateTour = (req, res) => {};
+
+const deleteTour = (req, res) => {};
+
+module.exports = {
+  getAllTours,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour,
+};
